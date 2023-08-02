a7346ec7055b17694e2ef141f7aec4bad4b3db65
Rey-Mermet Anthony <tzr2700@gmail.com>
2023-08-02 14:06:59 +0000

    functions added

--- models/sales.sql
+++ models/sales.sql
@@ -1,9 +1,7 @@
 {{ config(schema='transaction') }}
 
 WITH 
-
   sales AS (SELECT * FROM `gwz_raw_data.raw_gz_sales`)
-
   ,product AS (SELECT * FROM `gwz_raw_data.raw_gz_product`)
 
 SELECT
@@ -20,6 +18,7 @@ SELECT
   ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
 	,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
 	-- margin --
-	,s.revenue - s.quantity*CAST(p.purchSE_PRICE AS FLOAT64) AS margin
+    ,{{ margin_percent('s.revenue', 's.quantity * CAST(p.purchSE_PRICE AS FLOAT64)') }} AS product_margin_percent
+	,{{ margin ('s.revenue', 's.quantity * CAST(p.purchSE_PRICE AS FLOAT64)') }} AS product_margin
 FROM sales s
 INNER JOIN product p ON s.pdt_id = p.products_id
